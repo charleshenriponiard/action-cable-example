315c15f49965b6d8fc94e49ddb824f549dd85e58
charles-henri poniard <c.poniard@gmail.com>
2018-11-30 16:49:03 +0100

    add messages and chatroom controller, add chatrooms#show and partial to messages view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
   has_many :messages
   has_many :chatrooms, through: :messages
-  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true, uniqueness: true
 
 end
