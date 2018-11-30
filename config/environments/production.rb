118564f7ffe21c49bddc33247ea1e72ac8ff59f7
charles-henri poniard <c.poniard@gmail.com>
2018-11-30 18:06:53 +0100

    init heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  onfig.web_socket_server_url = "wss://action-cable-example-lewagon.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-example-lewagon.herokuapp.com', 'http://action-cable-example-lewagon.herokuapp.com']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
