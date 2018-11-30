1e7f536ac2d00a5a9844d4b3187f2e2a2340d5ad
charles-henri poniard <c.poniard@gmail.com>
2018-11-30 17:38:53 +0100

    add broadcast

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,18 @@
 class MessagesController < ApplicationController
+
   def create
-    @message = Message.new(message_params)
-    @message.user = current_user
+    message = Message.new(message_params)
+    message.user = current_user
     if message.save
-      # do some stuff
+      ActionCable.server.broadcast 'messages',
+        message: message.content,
+        user: message.user.first_name
+      head :ok
     else
       redirect_to chatrooms_path
     end
   end
+
 
   private
 
