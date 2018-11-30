8cac99b5d9f139ada3c0f6d5168dfba457588541
charles-henri poniard <c.poniard@gmail.com>
2018-11-30 17:09:10 +0100

    connection server side

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :chatrooms, only: [:show, :index]
-  resources :messages, only: [:show, :index]
+  mount ActionCable.server => '/cable'
+
+  resources :chatrooms, param: :slug
+  resources :messages
 
 end
